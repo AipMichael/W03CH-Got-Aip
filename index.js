af6d2019aed3eaf6f3713eca6fa3f083041ad050
AipBorges <mpbds@uic.es>
2021-10-11 13:32:00 +0200

    classes and subclasses created correctly without bugs

--- index.js
+++ index.js
@@ -5,76 +5,85 @@ class Personaje {
   estado = "vivo";
 
   constructor(nombre, familia, edad) {
-  nombre: nombre;
-  familia: familia;
-  edad: edad;
+    this.nombre = nombre;
+    this.familia = familia;
+    this.edad = edad;
   }
 
-  comunicar();
+  comunicar() {}
 
-  morir () {
+  morir() {
     this.estado = "muerto";
   }
 }
 
-class reina extends Personaje {
-  constructor(reinado, ) {
+class Reina extends Personaje {
+  constructor(nombre, familia, edad, reinado) {
     super(nombre, familia, edad);
-    this.reinado: reinado;
+    if (typeof reinado === "number") {
+      this.reinado = reinado;
+    }
   }
 
-  comunicar () {
+  comunicar() {
     return "Vais a morir todos";
   }
-
 }
 
-
-class asesora extends Personaje {
-  constructor(asesorada) {
+class Asesora extends Personaje {
+  constructor(nombre, familia, edad, asesorada) {
     super(nombre, familia, edad);
-    if (asesorada === reina || asesorada === asesora || asesorada === escudera || asesorada === luchadora){
-      this.asesorada: asesorada;
+    if (
+      asesorada instanceof Reina ||
+      asesorada instanceof Asesora ||
+      asesorada instanceof Escudera ||
+      asesorada instanceof Luchadora
+    ) {
+      this.asesorada = asesorada;
     }
-    
   }
 
-  comunicar () {
+  comunicar() {
     return "No sé por qué, pero creo que voy a morir pronto";
   }
-
 }
 
-class luchadora extends Personaje {
-  constructor(arma, destreza) {
+class Luchadora extends Personaje {
+  constructor(nombre, familia, edad, arma, destreza) {
     super(nombre, familia, edad);
     this.arma = arma;
-    if (typeof destreza === "number" && destreza < 10 && destreza > 10){
-      this.destreza: destreza;
+    if (typeof destreza === "number" && destreza < 10 && destreza > 10) {
+      this.destreza = destreza;
     }
   }
 
-  comunicar () {
+  comunicar() {
     return "Primero pego y luego pregunto";
   }
-
 }
 
-class escudera extends Personaje {
-  constructor(ama, pelotismo) {
+class Escudera extends Personaje {
+  constructor(nombre, familia, edad, ama, pelotismo) {
     super(nombre, familia, edad);
     this.ama = ama;
-    if (typeof pelotismo === "number" && pelotismo < 10 && pelotismo > 10){
-      this.pelotismo: pelotismo;
+    if (typeof pelotismo === "number" && pelotismo < 10 && pelotismo > 10) {
+      this.pelotismo = pelotismo;
     }
-    if (ama === reina || ama === asesora || ama === escudera || ama === luchadora){
-      this.ama: ama;
+    if (
+      ama === instancereina ||
+      ama === asesora ||
+      ama === escudera ||
+      ama === luchadora
+    ) {
+      this.ama = ama;
     }
-    
   }
 
-  comunicar () {
+  comunicar() {
     return "Soy una loser";
   }
-
 }
+
+const joffrey = new Reina("Joffrey", "Baratheon", 3);
+const jaime = new Luchadora("Jaime", "Lannister", 20, "honda", 7);
+console.log(jaime.morir());
